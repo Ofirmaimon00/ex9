b334be47730ec6ac3f45cedcdec7d98e7290be33
Ofirmaimon00 <ofirmaimon00@gmail.com>
2025-02-07 11:14:30 +0200

    added ofir's solution for q1

--- q1.sh
+++ q1.sh
@@ -1,1 +1,5 @@
-This is q1 answer
+awk '{print NR}' aliceinwonderland.txt
+awk 'BEGIN {count=0} {count += gsub(/ Alice /, "")} END {print count}' aliceinwonderland.txt
+awk '{for (i=1; i<=NF; i++) {word=tolower($i); gsub(/[^a-z]/, "", word); if (word != "") words[word]++}} END {for (w in words) if (words[w] == 1) print w}' aliceinwonderland.txt
+awk '{for (i=1; i<=NF; i++) {word=tolower($i); gsub(/[^a-z]/, "", word); if (word != "") words[word]++}} END {for (w in words) print words[w], w}' aliceinwonderland.txt | sort -nr | head -5
+awk '{for (i=1; i<=NF; i++) {word=$i; gsub(/[^a-zA-Z]/, "", word); total_length += length(word); total_words++}} END {print total_length / total_words}' aliceinwonderland.txt
